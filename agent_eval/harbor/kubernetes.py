4542beb0af49f3ede10951ba03f62f19198985ea
abhijeet-dhumal <abhijeetdhumal652@gmail.com>
2026-06-15 20:25:48 +0530

    fix: remove tini dependency
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>

--- agent_eval/harbor/kubernetes.py
+++ agent_eval/harbor/kubernetes.py
@@ -212,7 +212,7 @@ class KubernetesEnvironment(BaseEnvironment):
         container: dict = {
             "name": "main",
             "image": image,
-            "command": ["tini", "--", "sleep", "infinity"],
+            "command": ["sleep", "infinity"],
             "env": [{"name": k, "value": v} for k, v in env.items()],
             "resources": resources,
             "securityContext": {
